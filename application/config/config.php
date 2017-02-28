ed29ac873a59b970151d52e39e08b91d8e30ec0a
Roy Chung <rthug7@gmail.com>
2017-02-27 22:18:08 -0800

    Update #3-14

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://royboardphp.herokuapp.com/board/main/';
+$config['base_url'] = 'https://royboardphp.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
