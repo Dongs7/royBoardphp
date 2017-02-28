6cd71575bd92bec62b9b6f827602a58b2c01b085
Roy Chung <rthug7@gmail.com>
2017-02-27 22:22:25 -0800

    Update #3-15

--- application/views/header_v.php
+++ application/views/header_v.php
@@ -8,7 +8,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-  <link rel="stylesheet" href="public/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/public/css/bootstrap.min.css">
   <link rel="stylesheet" href="/public/css/master.css">
 
 
