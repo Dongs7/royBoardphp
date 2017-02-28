fc78efd66849d6bd3e14a1a8af58066dfe286bce
Roy Chung <rthug7@gmail.com>
2017-02-27 19:59:39 -0800

    Add public folder

--- application/views/header_v.php
+++ application/views/header_v.php
@@ -8,14 +8,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-  <link rel="stylesheet" href="/include/css/bootstrap.min.css">
-  <link rel="stylesheet" href="/include/css/master.css">
+  <link rel="stylesheet" href="/public/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/public/css/master.css">
 
 
-  <script type="text/javascript" src="/include/js/jquery.js"></script>
+  <script type="text/javascript" src="/public/js/jquery.js"></script>
   <script src="https://use.fontawesome.com/a932d2807f.js"></script>
-  <script type="text/javascript" src="/include/js/bootstrap.js"></script>
-  <script type="text/javascript" src="/include/js/master.js"></script>
+  <script type="text/javascript" src="/public/js/bootstrap.js"></script>
+  <!-- <script type="text/javascript" src="/public/js/master.js"></script> -->
 
   <title>roysBoard</title>
 </head>
