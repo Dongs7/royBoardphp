b66aa12ea2514daddd50d1a8ff2cc8c05a420df4
Roy Chung <rthug7@gmail.com>
2017-02-27 21:39:36 -0800

    Update #3-2

--- application/controllers/Board.php
+++ application/controllers/Board.php
@@ -23,6 +23,7 @@ class Board extends CI_Controller {
   {
     parent::__construct();
     date_default_timezone_set('America/Vancouver');
+    $this->load->model('Board_m');
   }
 
 
@@ -52,46 +53,46 @@ class Board extends CI_Controller {
 
   public function main()
   {
-    // $search_word  = $page_uri = '';
-		// $uri_segment = 3;
-    //
-		// //break down current uri address and put them into an array
-		// $uri_array = $this->check_uri($this->uri->uri_string());
-    //
-		// //If 'search' exists in the uri_array
-		// if(in_array('search',$uri_array))
-		// {
-		// 	$search_word = $uri_array[sizeof($uri_array)-1];
-		// 	$page_uri = '/search/'.$search_word;
-		// 	$uri_segment = 5;
-		// }
-    //
-		// $this->load->library('pagination');
-    //
-		// $config['base_url'] = '/board/main'.$page_uri;
-		// $config['total_rows'] = $this->board_m->get_list('count','','',$search_word);
-		// $config['per_page'] = 10;
-		// // $config['cur_tag_open'] = '<b>';
-    //
-		// $this->pagination->initialize($config);
-    //
-    // $data['pagination'] = $this->pagination->create_links();
-    //
-		// //If there is a value at 4th segment, allocate the value to the $page variable.
-		// //Otherwise, allocate 1 to the variable.
-		// $page = $this->uri->segment($uri_segment,1);
-		// if($page > 1)
-		// {
-		// 	$start = (($page/$config['per_page'])) * $config['per_page'];
-		// }
-		// else
-		// {
-		// 	$start = ($page - 1) * $config['per_page'];
-		// }
-    //
-		// $limit = $config['per_page'];
+    $search_word  = $page_uri = '';
+		$uri_segment = 3;
 
-		$data['list'] = $this->board_m->get_list('',$start, $limit,$search_word);
+		//break down current uri address and put them into an array
+		$uri_array = $this->check_uri($this->uri->uri_string());
+
+		//If 'search' exists in the uri_array
+		if(in_array('search',$uri_array))
+		{
+			$search_word = $uri_array[sizeof($uri_array)-1];
+			$page_uri = '/search/'.$search_word;
+			$uri_segment = 5;
+		}
+
+		$this->load->library('pagination');
+
+		$config['base_url'] = '/board/main'.$page_uri;
+		$config['total_rows'] = $this->board_m->get_list('count','','',$search_word);
+		$config['per_page'] = 10;
+		// $config['cur_tag_open'] = '<b>';
+
+		$this->pagination->initialize($config);
+
+    $data['pagination'] = $this->pagination->create_links();
+
+		//If there is a value at 4th segment, allocate the value to the $page variable.
+		//Otherwise, allocate 1 to the variable.
+		$page = $this->uri->segment($uri_segment,1);
+		if($page > 1)
+		{
+			$start = (($page/$config['per_page'])) * $config['per_page'];
+		}
+		else
+		{
+			$start = ($page - 1) * $config['per_page'];
+		}
+
+		$limit = $config['per_page'];
+
+		$data['list'] = $this->Board_m->get_list('',$start, $limit,$search_word);
 		$data['search_term'] = $search_word;
 		$this->load->view('board/List_v', $data);
   }
